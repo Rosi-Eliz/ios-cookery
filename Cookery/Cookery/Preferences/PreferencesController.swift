5d69718839d6ea3f4a1e3440e359390798c477cc
Vladislav Mazurov <vladdraculavm@gmail.com>
2021-05-19 14:34:40 +0200

    Add Preferences Screen

---
+++ Cookery/Cookery/Preferences/PreferencesController.swift
@@ -0,0 +1,22 @@
+//
+//  PreferencesController.swift
+//  Cookery
+//
+//  Created by Vladislav Mazurov on 19.05.21.
+//
+
+import UIKit
+
+class PreferencesController: UIViewController {
+
+    @IBOutlet weak var modifyButtonLabel: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        modifyButtonLabel.layer.cornerRadius = 10
+    }
+
+    @IBAction func modifyButtonPressed(_ sender: Any) {
+    }
+}
