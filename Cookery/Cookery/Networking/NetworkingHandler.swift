368400cce4399fab45b1921e48187a3ee680349d
Vladislav Mazurov <vladdraculavm@gmail.com>
2021-05-20 14:54:44 +0200

    Add missing files

---
+++ Cookery/Cookery/Networking/NetworkingHandler.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkingHandler.swift
+//  Cookery
+//
+//  Created by Rosi-Eliz Dzhurkova on 18.05.21.
+//
+
+import Foundation
+
+class NetwokringHandler {
+    private static let apiKey = "87a44ca0b30d4fba8aab771763b898a8"
+    enum Endpoint: String {
+        private static let baseURL = "https://api.spoonacular.com"
+        case randomRecipes
+        case foodIngredientsAutocomplete
+        
+        fileprivate var stringURL: String {
+            switch self {
+            case .randomRecipes: return Endpoint.baseURL.appending("/recipes/random")
+            case .foodIngredientsAutocomplete: return Endpoint.baseURL.appending("/food/ingredients/autocomplete")
+            }
+        }
+    }
+    
+    static func getRandomRecipes(withLimit limit: Int,
+                                 completion: @escaping((Result<RandomRecipeQuery, Error>) -> Void)){
+        var queryParameters = apiQueryParameter
+        queryParameters["number"] = "\(limit)"
+        NetworkingManager.shared.request(forUrl: Endpoint.randomRecipes.stringURL,
+                                         httpMethod: .get,
+                                         queryParameters: queryParameters,
+                                         completion: completion)
+    }
+    
+    static func getIngredientsForQuery(_ query: String,
+                                       completion: @escaping (Result<[Ingredient], Error>) -> Void) {
+        var queryParameters = apiQueryParameter
+        queryParameters["query"] = "\(query)"
+        NetworkingManager.shared.request(forUrl: Endpoint.foodIngredientsAutocomplete.stringURL,
+                                         httpMethod: .get,
+                                         queryParameters: queryParameters,
+                                         completion: completion)
+    }
+    
+    private static var apiQueryParameter: [String: String] {
+        return ["apiKey": apiKey]
+    }
+}
---
+++ Cookery/Cookery/Networking/NetworkingModels.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkingModels.swift
+//  Cookery
+//
+//  Created by Rosi-Eliz Dzhurkova on 18.05.21.
+//
+
+import Foundation
+
+struct RandomRecipeQuery: Decodable {
+    let recipes: [Recipes]
+}
+
+struct Recipes: Decodable {
+    let id: Int64
+    let title: String
+}
+
+struct Ingredient: StorageMainainable {
+    static var key = "Ingredients"
+    
+    let name: String
+    let image: String
+    
+    enum Size: Int {
+        case small = 100
+        case medium = 250
+        case large = 500
+    }
+    
+    func imageURL(for size: Size) -> String {
+        return "https://spoonacular.com/cdn/ingredients_\(size.rawValue)x\(size.rawValue)/\(image)"
+    }
+}
